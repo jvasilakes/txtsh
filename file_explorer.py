bb4cd3a5656c381c957c75dfcc647e9827233599
Jake Vasilakes <jvasilakes@gmail.com>
2014-09-26 22:22:11 +0100

    Fixed tabs and style.

--- file_explorer.py
+++ file_explorer.py
@@ -14,163 +14,139 @@ GO = 1
 CHOSEN = 2
 
 
-
 class Cursor(object):
 
     def __init__(self):
 
-	self.y = 4
-	self.x = 0
-
-	self.up_limit = 4
-	self.down_limit = 4
-
+        self.y = 4
+        self.x = 0
+        self.up_limit = 4
+        self.down_limit = 4
 
     def display(self):
 
-	curses.setsyx(self.y, self.x)
-	curses.doupdate()
-
+        curses.setsyx(self.y, self.x)
+        curses.doupdate()
 
 
 class Explorer(object):
 
     def __init__(self):
-	
-	self.scr = curses.initscr()
 
-	curses.noecho()
-	curses.curs_set(1)
-	self.scr.keypad(1)
+        self.scr = curses.initscr()
 
-	self.path = HOMEDIR
+        curses.noecho()
+        curses.curs_set(1)
+        self.scr.keypad(1)
 
-	self.curs = Cursor()
-
-	self.ls = os.listdir(self.path)
-
-	self.num_listings = len(self.ls)
-
-	self.curs.down_limit = self.num_listings + 3
-
-	self.current_file = self.ls[self.curs.y - 4]
-
+        self.path = HOMEDIR
+        self.curs = Cursor()
+        self.ls = os.listdir(self.path)
+        self.num_listings = len(self.ls)
+        self.curs.down_limit = self.num_listings + 3
+        self.current_file = self.ls[self.curs.y - 4]
 
     def manage_input(self, key):
 
-	if key == KEY_QUIT:
-	    return (STOP, None)
-    
-	elif key == KEY_CHOOSE_FILE:
-	    return (CHOSEN, self.path + self.current_file)
+        if key == KEY_QUIT:
+            return (STOP, None)
 
-	elif key == curses.KEY_UP:
+        elif key == KEY_CHOOSE_FILE:
+            return (CHOSEN, self.path + self.current_file)
 
-	    if self.curs.y == self.curs.up_limit:
-		pass
+        elif key == curses.KEY_UP:
 
-	    else:
-		self.curs.y -= 1
+            if self.curs.y == self.curs.up_limit:
+                pass
 
-	elif key == curses.KEY_DOWN:
+            else:
+                self.curs.y -= 1
 
-	    if self.curs.y == self.curs.down_limit:
-		pass
+        elif key == curses.KEY_DOWN:
+            if self.curs.y == self.curs.down_limit:
+                pass
+            else:
+                self.curs.y += 1
 
-	    else:
-		self.curs.y += 1
+        elif key == curses.KEY_RIGHT:
+            self.build_path()
+            self.curs.y = 4
 
-	elif key == curses.KEY_RIGHT:
+        elif key == curses.KEY_LEFT:
+            self.shrink_path()
+            self.curs.y = 4
 
-	    self.build_path()
+        else:
+            pass
 
-	    self.curs.y = 4
-
-	elif key == curses.KEY_LEFT:
-
-	    self.shrink_path()
-
-	    self.curs.y = 4
-
-	else:
-	    pass
-
-	return (GO, None)
-
+        return (GO, None)
 
     def list_dir(self):
 
-	max_y = self.scr.getmaxyx()[0] - 5
+        max_y = self.scr.getmaxyx()[0] - 5
 
-	try:
-	    self.ls = os.listdir(self.path)
+        try:
+            self.ls = os.listdir(self.path)
 
-	except:
-	    return
+            for i in xrange(len(self.ls)):
+                if self.ls[i].startswith('.'):
+                    self.ls.pop(i)
 
-	self.num_listings = len(self.ls)
+        except:
+            return
 
-	if self.num_listings == 0:
+        self.num_listings = len(self.ls)
 
-	    self.scr.addstr(4, 0, '*EMPTY*')
+        if self.num_listings == 0:
+            self.scr.addstr(4, 0, '*EMPTY*')
 
-	else:
+        else:
 
-	    self.scr.addstr(2, 0, self.path)
+            self.scr.addstr(2, 0, self.path)
 
-	    for i in xrange(len(self.ls)):
-		if i < max_y:
+            for i in xrange(len(self.ls)):
+                if i < max_y:
+                    self.scr.addstr(i + 4, 0, self.ls[i])
+                    self.scr.refresh()
 
-		    self.scr.addstr(i + 4, 0, self.ls[i])
-		    self.scr.refresh()
-
-	    self.curs.down_limit = self.num_listings + 3
-
+            self.curs.down_limit = self.num_listings + 3
 
     def build_path(self):
 
-	self.path = self.path + self.current_file + "/"
+        self.path = self.path + self.current_file + "/"
 
-    
     def shrink_path(self):
 
-	temp = self.path.split('/')
-
-	temp.pop(len(temp) - 2)
-
-	self.path = '/' + '/'.join([word for word in temp if word]) + '/'
-
+        temp = self.path.split('/')
+        temp.pop(len(temp) - 2)
+        self.path = '/' + '/'.join([word for word in temp if word]) + '/'
 
     def navigate(self):
-	
-	status = GO
 
-	c = None
-	while status == GO:
+        status = GO
 
-	    self.scr.clear()
+        c = None
+        while status == GO:
 
-	    self.scr.addstr(0, 0, "TXTSH FILE EXPLORER")
-	    self.scr.addstr(1, 0, "'c' to choose file, 'q' to quit without choosing.")
+            self.scr.clear()
+            self.scr.addstr(0, 0, "TXTSH FILE EXPLORER")
+            self.scr.addstr(1, 0, "'c' to choose file, \
+                    'q' to quit without choosing.")
 
-	    self.list_dir()
+            self.list_dir()
+            self.curs.display()
+            self.current_file = self.ls[self.curs.y - 4]
 
-	    self.curs.display()
+            c = self.scr.getch()
+            (status, path) = self.manage_input(c)
 
-	    self.current_file = self.ls[self.curs.y - 4]
+        curses.endwin()
 
-	    c = self.scr.getch()
-
-	    (status, path) = self.manage_input(c)
-
-	curses.endwin()
-
-	return path
+        return path
 
 
 if __name__ == '__main__':
     expl = Explorer()
     path = expl.navigate()
     if path:
-	print path
-
+        print path
