3a419968c66d3801a01bd3f3dc2a6bc8964e584a
Jake Vasilakes <jvasilakes@gmail.com>
2014-09-26 22:32:25 +0100

    Fixed tabs and style.

--- text.py
+++ text.py
@@ -1,4 +1,15 @@
-import nltk
+import sys
+
+from log import Log
+
+try:
+    import nltk
+    Log.get().writeLog("nltk successfully imported.")
+
+except Exception as e:
+    Log.get().writeLog(e)
+    print e
+    sys.exit(0)
 
 
 class Text(object):
@@ -7,67 +18,52 @@ class Text(object):
 
     def __init__(self):
 
-	self.filepath = None
+        self.filepath = None
+        self.id = self.set_id()
+        self.title = ''
+        self.contents = ''
+        self.words = {}
+        self.numbers = {}
+        self.punctuation = {}
+        self.members.append(self)
 
-	self.id = self.set_id()
-
-	self.title = ''
-
-	self.contents = ''
-
-	self.words = {}
-
-	self.numbers = {}
-
-	self.punctuation = {}
-
-	self.members.append(self)
-
-    
     def set_id(self):
-    
-	if self.members:
-	    return (self.members[(len(self.members) - 1)].id) + 1
 
-	else:
-	    return 1
+        if self.members:
+            return (self.members[(len(self.members) - 1)].id) + 1
 
+        else:
+            return 1
 
     def load_data(self, data_type, data):
 
-	if data_type == 'file':
+        if data_type == 'file':
+            self.filepath = data
 
-	    self.filepath = data
+            try:
+                with open(data, 'r') as f:
+                    data = f.read()
 
-	    try:
-		with open(data, 'r') as f:
-		    data = f.read()
+            except:
+                print "Could not read from file: %s." % data
+                return GO
 
-	    except:
-		print "Could not read from file: %s." % data
+        self.title = data[:20]
+        self.contents = data
+        temp = nltk.word_tokenize(data)
 
-		return GO
+        for item in temp:
 
+            if item[0].isalpha():
+                l = self.words
+            elif item.isdigit():
+                l = self.numbers
+            else:
+                l = self.punctuation
 
-	self.title = data[:20]
+            if item in l:
+                l[item] += 1
+            else:
+                l.update({item: 1})
 
-	self.contents = data
-
-	temp = nltk.word_tokenize(data)
-
-	for item in temp:
-
-	    if item[0].isalpha():
-		l = self.words
-	    elif item.isdigit():
-		l = self.numbers
-	    else:
-		l = self.punctuation
-
-	    if item in l:
-		l[item] += 1
-	    else:
-		l.update({item: 1})
-
-	print "Data loaded into id %d." % self.id
-
+        print "Data loaded into id %d." % self.id
