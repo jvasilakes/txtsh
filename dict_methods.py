c848e6852696d551752af11c1eebb1c59c056e21
Jake Vasilakes <jvasilakes@gmail.com>
2014-09-26 22:17:33 +0100

    Fixed tabs and style.

--- dict_methods.py
+++ dict_methods.py
@@ -1,16 +1,15 @@
 def getMaxKey(d):
 
     if not isinstance(d, dict):
-	print "Input must be a dictionary."
+        print "Input must be a dictionary."
 
     word = d.keys()[0]
 
     count = 0
 
     for key in d:
-	if d[key] > count:
-	    count = d[key]
-	    word = key
+        if d[key] > count:
+            count = d[key]
+            word = key
 
     return word, count
-
