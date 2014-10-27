6d4dd351b5a2c105c29df7070098885e4d226c98
Jake Vasilakes <jvasilakes@gmail.com>
2014-10-27 12:36:20 -0500

    New, more verbose way to read in files.

--- text.py
+++ text.py
@@ -28,16 +28,13 @@ class Text(object):
 
     def load_data(self, data_type, data):
 
-        if data_type == 'file':
-            self.filepath = data
+        if data_type.lower() == 'file':
+            data = self.readfile(data)
 
-            try:
-                with open(data, 'r') as f:
-                    data = f.read()
-
-            except:
-                print "Could not read from file: {}." .format(data)
-                return GO
+            if not data:
+                log.write("Could not read from {}" .format(self.filepath))
+                print "Could not read from {}" .format(self.filepath)
+                return
 
         # Get rid of unwanted newlines in the title.
         self.title = re.sub(r'\s+', ' ', data[:20])
@@ -59,3 +56,15 @@ class Text(object):
                 l.update({item: 1})
 
         print "Data loaded into id {}." .format(self.id)
+
+    def readfile(self, data):
+
+        self.filepath = data
+
+        try:
+            with open(data, 'r') as f:
+                data = f.read()
+                return data
+
+        except:
+            return None
