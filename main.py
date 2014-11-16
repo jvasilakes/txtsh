d82d2db56144e3c15e27bac69225fd5bbf3faf5d
Jake Vasilakes <jvasilakes@gmail.com>
2014-11-15 18:53:24 -0600

    Take advantage of new verbose option.

--- main.py
+++ main.py
@@ -1,21 +1,30 @@
 #! /usr/bin/env python
 
+import sys
+
 import txtsh.log as log
 from txtsh.shell import Shell
 
 
-def main():
+def main(argv):
     """
     The main driver function, which starts the shell.
+
+    options:
+        -v  verbose
     """
     log.start('.txtsh_log')
     log.write(mes="Txtsh started.")
 
-    sh = Shell()
+    if argv[-1] == '-v':
+        sh = Shell(verbose=True)
+    else:
+        sh = Shell()
+
     sh.run()
 
     return
 
 
 if __name__ == '__main__':
-        main()
+    main(sys.argv)
